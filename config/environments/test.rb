6b9c47f13a4bfad801369f256f3859106d3a1050
scara82 <marta.scaramella@gmail.com>
2017-12-04 16:00:41 +1100

    Setup Db cleaner

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -4,7 +4,6 @@ Rails.application.configure do
 
   config.eager_load = false
 
-  # Configure public file server for tests with Cache-Control for performance.
   config.public_file_server.enabled = true
   config.public_file_server.headers = {
     'Cache-Control' => "public, max-age=#{1.hour.seconds.to_i}"
