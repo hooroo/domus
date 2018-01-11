cb907f6a6face5e3ff669845c3adb49df83d566e
scara82 <marta.scaramella@gmail.com>
2018-01-11 16:53:37 +1100

    Add family model specs for new family validation

--- app/controllers/families_controller.rb
+++ app/controllers/families_controller.rb
@@ -1,2 +1,26 @@
 class FamiliesController < ApplicationController
+  def new
+  end
+
+  def create
+    family = Family.new
+    family.first_name = params[:first_name]
+    family.last_name = params[:last_name]
+    family.address = params[:address]
+    family.animals = params[:animals]
+    family.capability = params[:capability]
+    family.prof = params[:prof]
+    family.preffered_gender = params[:preffered_gender]
+    family.bus_driver = params[:bus_driver]
+    family.active = params[:active]
+
+    if family.save
+      redirect_to '/'
+    else
+      #
+    end
+  end
+
+  def destroy
+  end
 end
--- spec/models/family_spec.rb
+++ spec/models/family_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
-RSpec.describe Family, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Family do
+  it "is not valid without a last_name" do
+    new_family = Family.new(last_name: nil)
+
+    expect(new_family).to_not be_valid
+  end
+
+  it "is not valid without capability" do
+    new_family = Family.new(capability: nil)
+
+    expect(new_family).to_not be_valid
+  end
+
+  it "is valid without a first_name" do
+    new_family = Family.new(
+      first_name: nil,
+      last_name: "Marta",
+      capability: 2
+    )
+
+    expect(new_family).to be_valid
+  end
 end
