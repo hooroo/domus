197d2aeaa39039a6096dfd0effa94313b7ca864f
scara82 <marta.scaramella@gmail.com>
2018-01-12 13:48:53 +1100

    Add models family spec

--- spec/models/family_spec.rb
+++ spec/models/family_spec.rb
@@ -1,25 +1,27 @@
 require 'rails_helper'
 
 describe Family do
-  it "is not valid without a last_name" do
-    new_family = Family.new(last_name: nil)
+  describe "#create" do
+    it "is not valid without a last_name" do
+      new_family = Family.new(last_name: nil)
 
-    expect(new_family).to_not be_valid
-  end
+      expect(new_family).to_not be_valid
+    end
 
-  it "is not valid without capability" do
-    new_family = Family.new(capability: nil)
+    it "is not valid without capability" do
+      new_family = Family.new(capability: nil)
 
-    expect(new_family).to_not be_valid
-  end
+      expect(new_family).to_not be_valid
+    end
 
-  it "is valid without a first_name" do
-    new_family = Family.new(
-      first_name: nil,
-      last_name: "Marta",
-      capability: 2
-    )
+    it "is valid without a first_name" do
+      new_family = Family.new(
+        first_name: nil,
+        last_name: "Marta",
+        capability: 2
+      )
 
-    expect(new_family).to be_valid
+      expect(new_family).to be_valid
+    end
   end
 end
