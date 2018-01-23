464f4aa67458b23042f0ebb87db0e32d84ab4515
scara82 <marta.scaramella@gmail.com>
2018-01-23 16:04:29 +1100

    Eliminates the shoulda_matcher configuration that is in spec/support/shoulda_matcher.rb

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,19 +7,7 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
 ActiveRecord::Migration.maintain_test_schema!
 
-Shoulda::Matchers.configure do |config|
-  config.integrate do |with|
-    with.test_framework :rspec
-    with.library :rails
-  end
-end
-
-
 RSpec.configure do |config|
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
-  config.use_transactional_fixtures = true
-
-  config.infer_spec_type_from_file_location!
-
-  config.filter_rails_from_backtrace!
+  config.color = true
+  config.tty = true
 end
