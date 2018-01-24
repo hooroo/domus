8cc871b18815770492a983f2db7181c0cbf771b6
scara82 <marta.scaramella@gmail.com>
2018-01-24 16:34:38 +1100

    Changes gemfile and rails_helper to fix shoulda-matchers

--- Gemfile
+++ Gemfile
@@ -32,7 +32,7 @@ group :test do
   gem "database_cleaner"
   gem "launchy"
   gem "rails-controller-testing"
-  gem "shoulda-matchers", git: "https://github.com/thoughtbot/shoulda-matchers.git", branch: "rails-5"
+  gem "shoulda-matchers", "~> 3.1"
   gem "timecop"
   gem "webmock"
 end
--- spec/models/family_spec.rb
+++ spec/models/family_spec.rb
@@ -1,27 +1,7 @@
-require 'rails_helper'
+require "rails_helper"
 
 describe Family do
-  describe "#create" do
-    it "is not valid without a last_name" do
-      new_family = Family.new(last_name: nil)
-
-      expect(new_family).to_not be_valid
-    end
-
-    it "is not valid without capability" do
-      new_family = Family.new(capability: nil)
-
-      expect(new_family).to_not be_valid
-    end
-
-    it "is valid without a first_name" do
-      new_family = Family.new(
-        first_name: nil,
-        last_name: "Marta",
-        capability: 2
-      )
-
-      expect(new_family).to be_valid
-    end
+  describe "validations" do
+    it { is_expected.to validate_presence_of(:last_name) }
   end
 end
