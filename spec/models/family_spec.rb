5615a79e161c3eaa608d938ebca11ce634b95c4c
scara82 <marta.scaramella@gmail.com>
2018-01-24 23:28:46 +1100

    Adds specs for models/family and factories

--- spec/models/family_spec.rb
+++ spec/models/family_spec.rb
@@ -2,6 +2,18 @@ require "rails_helper"
 
 describe Family do
   describe "validations" do
-    it { is_expected.to validate_presence_of(:last_name) }
+    subject { Family.new(last_name: nil) }
+
+    context "when creating a new family with no last_name is not valid" do
+      it { should_not allow_value(nil).for(:last_name)}
+    end
+
+    context "when creating a new family with no capability is not valid" do
+      it { should_not allow_value(nil).for(:capability)}
+    end
+
+    context "when creating a new family that is valid it is saved" do
+      it { expect(build(:family)).to be_valid }
+    end
   end
 end
