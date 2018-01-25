99411e8f4ef77359793278c3f640bae91cb7ab1f
scara82 <marta.scaramella@gmail.com>
2018-01-25 20:54:59 +1100

    Add validation specs for family model

--- spec/models/family_spec.rb
+++ spec/models/family_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe Family, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Family do
+  describe "validations" do
+    subject { Family.new(last_name: nil) }
+
+    context "when creating a new family with no last_name is not valid" do
+      it { should_not allow_value(nil).for(:last_name)}
+    end
+
+    context "when creating a new family with no capability is not valid" do
+      it { should_not allow_value(nil).for(:capability)}
+    end
+
+    context "when creating a new family that is valid it is saved" do
+      it { expect(build(:family)).to be_valid }
+    end
+  end
 end
