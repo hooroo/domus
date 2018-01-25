b8b249657b64a2df8a928e1d11bc95a6712161d3
scara82 <marta.scaramella@gmail.com>
2018-01-25 20:35:43 +1100

    Add family_spec.rb and a family at the factorie.rb for testing

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,2 +1,13 @@
-FactoryBot.define do  
+FactoryBot.define do
+  factory :family do
+    first_name "Ro"
+    last_name "Liso"
+    address "San Cesareo"
+    animals "No"
+    capability 3
+    prof false
+    preferred_gender "female"
+    bus_driver false
+    active true
+  end
 end
