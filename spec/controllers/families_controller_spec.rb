b43b0c0a1bfffff9cec239d9a708543d2ca94437
scara82 <marta.scaramella@gmail.com>
2018-01-25 15:16:45 +1100

    Add spec/controller/families_controller_spec.rb

--- spec/controllers/families_controller_spec.rb
+++ spec/controllers/families_controller_spec.rb
@@ -1,5 +1,22 @@
 require "rails_helper"
 
-RSpec.describe FamiliesController, type: :controller do
+RSpec.describe FamiliesController do
+  describe "GET /index" do
+    it "is successful" do
+      get :index
 
+      expect(response).to be_success
+    end
+
+    it "renders the :index view" do
+      get :index
+
+      expect(response).to render_template :index
+    end
+    
+    it "returns all the families" do
+      create_list(:family, 2)
+    end
+
+  end
 end
