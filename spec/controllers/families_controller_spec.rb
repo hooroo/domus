34d734fc2510771d1e5d561a50f1c51691c1f639
scara82 <marta.scaramella@gmail.com>
2018-01-26 13:06:50 +1100

    Add specs for families#create

--- spec/controllers/families_controller_spec.rb
+++ spec/controllers/families_controller_spec.rb
@@ -16,4 +16,22 @@ RSpec.describe FamiliesController do
       end
     end
   end
+  
+  describe "POST /families" do
+    it "creates a new family with correct attribute" do
+      family_params = attributes_for(:family, last_name: "Scaramella", capability: "2")
+
+      post :create, params: { family: family_params }
+
+      family = Family.last
+
+      expect(family.last_name).to eq "Scaramella"
+    end
+
+    it "increases the count of the families by one" do
+      family_params = attributes_for(:family, last_name: "Scaramella", capability: "2")
+
+      expect { post :create, params: { family: family_params } }.to change(Family, :count).by(1)
+    end
+  end
 end
