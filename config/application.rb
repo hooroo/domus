f7a5f7ca73bd967ab4266dbeb517a8ba52ba10fd
scara82 <marta.scaramella@gmail.com>
2018-01-26 13:05:52 +1100

    Configure responders

--- config/application.rb
+++ config/application.rb
@@ -15,14 +15,11 @@ Bundler.require(*Rails.groups)
 
 module Domus
   class Application < Rails::Application
+    config.app_generators.scaffold_controller :responders_controller
 
-    config.middleware.use ::Rack::Auth::Basic do |u, p|
-      [u, p] == [ENV["BASIC_AUTH_USERNAME"], ENV["BASIC_AUTH_PASSWORD"]]
-    end
-
+    config.action_controller.action_on_unpermitted_parameters = :raise
     config.load_defaults 5.1
     config.generators.system_tests = nil
-    config.action_controller.action_on_unpermitted_parameters = :raise
 
     config.generators do |generate|
       generate.helper false
