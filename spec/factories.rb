4c2e3668b5defc9b4666785c6d8c08000a8e98d5
scara82 <marta.scaramella@gmail.com>
2018-01-29 16:58:20 +1100

    Eliminate from the factory the attributes don't needed for validation

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,13 +1,6 @@
 FactoryBot.define do
   factory :family do
-    first_name "Ro"
     last_name "Liso"
-    address "San Cesareo"
-    animals "No"
     capability 3
-    prof false
-    preferred_gender "female"
-    bus_driver false
-    active true
   end
 end
