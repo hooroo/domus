9188b0c5593549a44986754d4afcb27b827d56b3
scara82 <marta.scaramella@gmail.com>
2018-02-01 11:56:14 +1100

    Add features spec folder and features/families_spec.rb file

--- spec/controllers/families_controller_spec.rb
+++ spec/controllers/families_controller_spec.rb
@@ -1,5 +1,6 @@
 require "rails_helper"
 
+# controller specs for the sad path - i.e. trying to create a new family without required attributes => testing to see that the flash message was set correctly, and the rendered template is :new rather than :index, and that the object has error messages on it
 RSpec.describe FamiliesController do
   describe "POST /families" do
     context "with invalid params" do
@@ -14,24 +15,6 @@ RSpec.describe FamiliesController do
           post :create, params: { family: family_params }
         end.to change(Family, :count).by(0)
       end
-    end
-  end
-  
-  describe "POST /families" do
-    it "creates a new family with correct attribute" do
-      family_params = attributes_for(:family, last_name: "Scaramella", capability: "2")
-
-      post :create, params: { family: family_params }
-
-      family = Family.last
-
-      expect(family.last_name).to eq "Scaramella"
-    end
-
-    it "increases the count of the families by one" do
-      family_params = attributes_for(:family, last_name: "Scaramella", capability: "2")
-
-      expect { post :create, params: { family: family_params } }.to change(Family, :count).by(1)
     end
   end
 end
