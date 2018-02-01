af7465b3f3aed71506df36e241eb7fb3f20edffc
scara82 <marta.scaramella@gmail.com>
2018-02-02 09:32:42 +1100

    Add features spec for adding a family

---
+++ spec/features/admin_adds_a_family_spec.rb
@@ -0,0 +1,16 @@
+require "rails_helper"
+
+feature "Admin adds a family" do
+  scenario "successfully" do
+    visit new_family_path
+
+    fill_in "family_last_name", with: "Scaramella"
+    fill_in "family_capability", with: 2
+    click_on I18n.t("families.new.submit")
+
+    expect(page).to have_text(
+      I18n.t("flash.actions.create.notice", resource_name: "Family")
+    )
+    expect(current_path).to eq families_path
+  end
+end
