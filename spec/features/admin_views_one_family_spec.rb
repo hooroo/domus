3f9cfd3e51b1c7cf7fd18e4f77cfed2400288ada
scara82 <marta.scaramella@gmail.com>
2018-02-04 14:42:04 +1100

    Modify the test, visit the correct page and add test for presence of the title and family last_name

--- spec/features/admin_views_one_family_spec.rb
+++ spec/features/admin_views_one_family_spec.rb
@@ -2,8 +2,11 @@ require "rails_helper"
 
 feature "Admin can see a family" do
   scenario "successfully" do
-    visit family_path
+    family = create(:family, last_name: "Yellow")
+
+    visit "/families/#{family.id}"
 
     expect(page).to have_text(I18n.t("families.show.title"))
+    expect(page).to have_text("Yellow")
   end
 end
