11aafd0fa17e4da9d7dabd8c922b8710fb0cb298
scara82 <marta.scaramella@gmail.com>
2018-02-06 13:42:38 +1100

    Regenrate schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -6,11 +6,12 @@
 # database schema. If you need to create the application database on another
 # system, you should be using db:schema:load, not running all the migrations
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you"ll amass, the slower it"ll run and the greater likelihood for issues).
+# you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It"s strongly recommended that you check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180125095124) do
+ActiveRecord::Schema.define(version: 20180125091555) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,11 +21,12 @@ ActiveRecord::Schema.define(version: 20180125095124) do
     t.string "address"
     t.string "animals"
     t.string "capability", null: false
+    t.string "preferred_gender"
     t.boolean "prof", default: false
     t.boolean "bus_driver", default: false
     t.boolean "active", default: true
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "preferred_gender"
   end
+
 end
