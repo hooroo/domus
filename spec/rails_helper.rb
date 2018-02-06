fd1009c153c18996bb1179322f0cc980518920cc
scara82 <marta.scaramella@gmail.com>
2018-02-06 13:43:43 +1100

    Clean rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,4 @@
 require "spec_helper"
-# require "capybara/rspec"
-
 ENV["RAILS_ENV"] ||= "test"
 require File.expand_path("../../config/environment", __FILE__)
 
@@ -17,8 +15,6 @@ ActiveRecord::Migration.maintain_test_schema!
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = true
-
   config.infer_spec_type_from_file_location!
-
   config.filter_rails_from_backtrace!
 end
