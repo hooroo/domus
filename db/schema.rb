30bca1e66aa685f151fdd0ff8ad87b35b3186666
scara82 <marta.scaramella@gmail.com>
2018-02-06 16:28:39 +1100

    Eliminate empty lines

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,8 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20180125091555) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
   create_table "families", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name", null: false
@@ -28,5 +26,4 @@ ActiveRecord::Schema.define(version: 20180125091555) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 end
