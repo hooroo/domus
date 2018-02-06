c0efe4b702d021913c7be0d5b884c74a1a0710d7
scara82 <marta.scaramella@gmail.com>
2018-02-06 16:28:16 +1100

    Change Rubocop configurations for methods length and trailing comma

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,6 @@
 ActiveRecord::Schema.define(version: 20180125091555) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
   create_table "families", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name", null: false
