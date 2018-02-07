995587e83d35e50e370c58067a745ab162ea97dd
scara82 <marta.scaramella@gmail.com>
2018-02-07 18:07:29 +1100

    Add update family feature with specs

---
+++ spec/features/admin_updates_family_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+feature "Admin updates a family" do
+  scenario "successfully" do
+    family = create(:family)
+
+    visit edit_family_path(family)
+
+    fill_in "family_last_name", with: "New Last Name"
+    click_on I18n.t("families.form.submit")
+
+    expect(page).to have_text(
+      I18n.t("flash.actions.update.notice", resource_name: "Family")
+    )
+    expect(page).to have_text "New Last Name"
+  end
+end
