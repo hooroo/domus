c0d53b8bdb3ab4d14c6e71153ebeadba17d1d890
scara82 <marta.scaramella@gmail.com>
2018-02-25 12:24:28 +1100

    Updates the header with the correct links, adds homepage and restyle

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :families
   resources :schools, only: [:index, :new, :create, :edit, :update, :destroy]
   resources :trips, only: [:index, :new, :create, :edit, :update, :destroy]
+  resources :pages, only: [:show]
 
-  root "families#index"
+  root "pages#show"
 end
