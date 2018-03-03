4822d0c5c188b07fae061bfe1c0f6abe2363c590
scara82 <marta.scaramella@gmail.com>
2018-03-03 17:42:32 +1100

    Add features specs for happy path when admin click on notActive/Active/All in the index page and wants to see the different families lists

--- spec/features/families/admin_views_all_families_spec.rb
+++ spec/features/families/admin_views_all_families_spec.rb
@@ -1,9 +1,19 @@
 require "rails_helper"
 
-feature "Admin can see all the families in the homepage" do
-  scenario "successfully" do
-    visit root_path
+feature "Admin can see all the families when in families#index" do
+  scenario "when click on the 'All' button" do
+    scenario "successfully" do
+      create(:family, active: false, last_name: "Scara")
+      create(:family, active: true, last_name: "Liso")
 
-    expect(page).to have_text(I18n.t("families.index.title"))
+      visit families_path
+      find("#all_families_button").click
+
+      expect(page).to have_text(I18n.t("families.index.all_families"))
+      expect(page).to have_text("false")
+      expect(page).to have_text("Scara")
+      expect(page).to have_text("true")
+      expect(page).to have_text("Liso")
+    end
   end
 end
