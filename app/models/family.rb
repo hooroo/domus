ef30c24ab2d00df3fc8ebd8474101162ac7793b0
scara82 <marta.scaramella@gmail.com>
2018-03-03 17:43:01 +1100

    Change translations

--- app/models/family.rb
+++ app/models/family.rb
@@ -1,6 +1,14 @@
 class Family < ApplicationRecord
   validates_presence_of :last_name, :capability
 
+  def self.active
+    where(active: true)
+  end
+
+  def self.inactive
+    where(active: false)
+  end
+
   scope :active, -> { where("active = true") }
   scope :not_active, -> { where("active = false") }
 
