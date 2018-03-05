00152c7b8e56564d17196069bdb5c2078ad78f4e
scara82 <marta.scaramella@gmail.com>
2018-03-06 08:47:34 +1100

    Rename not_active_families into inactive_families

--- app/models/family.rb
+++ app/models/family.rb
@@ -1,16 +1,8 @@
 class Family < ApplicationRecord
   validates_presence_of :last_name, :capability
 
-  def self.active
-    where(active: true)
-  end
-
-  def self.inactive
-    where(active: false)
-  end
-
   scope :active, -> { where("active = true") }
-  scope :not_active, -> { where("active = false") }
+  scope :inactive, -> { where("active = false") }
 
   def self.active
     where(active: true)
