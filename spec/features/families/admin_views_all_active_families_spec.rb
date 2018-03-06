52b2eef72a6777bffa23e2625523adc852e627a3
scara82 <marta.scaramella@gmail.com>
2018-03-07 09:20:12 +1100

    Add all_true_families and all_false_families traits into factories and use it in the feature specs

--- spec/features/families/admin_views_all_active_families_spec.rb
+++ spec/features/families/admin_views_all_active_families_spec.rb
@@ -1,9 +1,15 @@
 require "rails_helper"
 
 feature "Admin can see all the active families when in families#index" do
-  scenario "successfully" do
+  scenario "successfully", js: true do
+    create(:all_true_family)
+    create(:all_false_family)
+
     visit families_path
+    find("#active_families_button").click
 
     expect(page).to have_text(I18n.t("families.index.buttons.active"))
+    expect(page).to have_text(true)
+    expect(page).not_to have_text(false)
   end
 end
--- spec/features/families/admin_views_all_families_spec.rb
+++ spec/features/families/admin_views_all_families_spec.rb
@@ -1,19 +1,24 @@
 require "rails_helper"
 
 feature "Admin can see all the families when in families#index" do
+<<<<<<< HEAD
   context "when click on the 'All' button" do
     scenario "successfully" do
       create(:family, active: false, last_name: "Scara")
       create(:family, active: true, last_name: "Liso")
+=======
+  describe "when click on the 'All' button" do
+    scenario "successfully", js: true do
+      create(:all_true_family)
+      create(:all_false_family)
+>>>>>>> b6c4d17... Add all_true_families and all_false_families traits into factories and use it in the feature specs
 
       visit families_path
       find("#all_families_button").click
 
       expect(page).to have_text(I18n.t("families.index.buttons.all"))
       expect(page).to have_text("false")
-      expect(page).to have_text("Scara")
       expect(page).to have_text("true")
-      expect(page).to have_text("Liso")
     end
   end
 end
