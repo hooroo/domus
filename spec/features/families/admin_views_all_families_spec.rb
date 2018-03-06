fe95721050375543854c0613dbbc350f316f6cac
Elle Meredith <ellemeredith@gmail.com>
2018-03-06 11:58:01 +1100

    Clean up how tabs are being displayed
    - Dried the JS methods by removing the duplication and made them more
      generic so the script can be re-used.
    - Move the control of show/hide tabs to css, by using an `.active` class
    - And move the js click event to be called from the javascript file
    - To make that happen we need a `.sub-header .button` with a `data-target` attribute.
      We also need a `.tabs` area with nested `.tab `,
      and a class that matches the `data-target`
    - What the JS does is pretty much hide all the tabs with a class of `.tab`
      and then only shows the one with the right class name
    - Removed the feature specs because if JS is disabled,
      the user will still be able to see the families groupings.
    - Removed the `sub_header` partial because it was only used in one place
      and it was simpler to keep the markup in the same view

--- spec/features/families/admin_views_all_families_spec.rb
+++ spec/features/families/admin_views_all_families_spec.rb
@@ -1,17 +1,10 @@
 require "rails_helper"
 
 feature "Admin can see all the families when in families#index" do
-<<<<<<< HEAD
-  context "when click on the 'All' button" do
-    scenario "successfully" do
-      create(:family, active: false, last_name: "Scara")
-      create(:family, active: true, last_name: "Liso")
-=======
   describe "when click on the 'All' button" do
     scenario "successfully", js: true do
       create(:all_true_family)
       create(:all_false_family)
->>>>>>> b6c4d17... Add all_true_families and all_false_families traits into factories and use it in the feature specs
 
       visit families_path
       find("#all_families_button").click
