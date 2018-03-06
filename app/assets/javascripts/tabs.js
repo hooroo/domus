d633d0d38cc05f7561677ab203d9dd5d2ea62197
Elle Meredith <ellemeredith@gmail.com>
2018-03-06 12:50:18 +1100

    Change buttons to be links, and clean their styling
    - Semantically, we click either links or actual form buttons
      so, changed the markup to be more semantically correct.
    - Thus added `e.preventDefault();`
      so that the JS will not follow the link through.
    - Also now adding the `.active` class to the buttons as well,
      to indicate which one is currently active.
    - Cleaned up the styles to use sass, and incorporate the changes in
      markup. Also added a style for hovering the links.
    
    Suggested TODO:
    What would be good for the UX, is to actually
    have the buttons to link to actual views.
    So if the user has JS disabled, they can still see each group.
    
    Suggested consideration:
    Start thinking about how to handle when there are too many families.
    One possible solution can be pagination.

--- app/assets/javascripts/tabs.js
+++ app/assets/javascripts/tabs.js
@@ -2,17 +2,12 @@ const buttons = document.querySelectorAll(".sub-header .button");
 const tabs = document.querySelectorAll(".tab");
 
 function toggleActive(e) {
-<<<<<<< HEAD
   e.preventDefault();
   target_class_name = e.target.dataset["target"];
 
   buttons.forEach(button => button.classList.remove("active"));
   tabs.forEach(tab => tab.classList.remove("active"));
   e.target.classList.add("active");
-=======
-  target_class_name = e.target.dataset["target"];
-  tabs.forEach(tab => tab.classList.remove("active"));
->>>>>>> 97f1151... Clean up how tabs are being displayed
   document.querySelector(`.tab.${target_class_name}`).classList.add("active");
 }
 
