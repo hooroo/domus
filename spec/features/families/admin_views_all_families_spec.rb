95891ab8f8181077106c554d4e1c0350a90b4d10
scara82 <marta.scaramella@gmail.com>
2018-03-07 09:20:12 +1100

    Add all_true_families and all_false_families traits into factories and use it in the feature specs

--- spec/features/families/admin_views_all_families_spec.rb
+++ spec/features/families/admin_views_all_families_spec.rb
@@ -2,18 +2,16 @@ require "rails_helper"
 
 feature "Admin can see all the families when in families#index" do
   describe "when click on the 'All' button" do
-    scenario "successfully" do
-      create(:family, active: false, last_name: "Scara")
-      create(:family, active: true, last_name: "Liso")
+    scenario "successfully", js: true do
+      create(:all_true_family)
+      create(:all_false_family)
 
       visit families_path
       find("#all_families_button").click
 
       expect(page).to have_text(I18n.t("families.index.buttons.all"))
       expect(page).to have_text("false")
-      expect(page).to have_text("Scara")
       expect(page).to have_text("true")
-      expect(page).to have_text("Liso")
     end
   end
 end
