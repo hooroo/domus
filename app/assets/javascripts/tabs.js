c9f10fcfd49ba9b7067420d01914a20c66c86704
Elle Meredith <ellemeredith@gmail.com>
2018-03-06 11:58:01 +1100

    Clean up how tabs are being displayed
    - Dried the JS methods by removing the duplication and made them more
      generic so the script can be re-used.
    - Move the control of show/hide tabs to css, by using an `.active` class
    - And move the js click event to be called from the javascript file
    - To make that happen we need a `.sub-header .button` with a `data-target` attribute.
      We also need a `.tabs` area with nested `.tab `,
      and a class that matches the `data-target`
    - What the JS does is pretty much hide all the tabs with a class of `.tab`
      and then only shows the one with the right class name
    - Removed the feature specs because if JS is disabled,
      the user will still be able to see the families groupings.
    - Removed the `sub_header` partial because it was only used in one place
      and it was simpler to keep the markup in the same view

--- app/assets/javascripts/tabs.js
+++ app/assets/javascripts/tabs.js
@@ -2,12 +2,17 @@ const buttons = document.querySelectorAll(".sub-header .button");
 const tabs = document.querySelectorAll(".tab");
 
 function toggleActive(e) {
+<<<<<<< HEAD
   e.preventDefault();
   target_class_name = e.target.dataset["target"];
 
   buttons.forEach(button => button.classList.remove("active"));
   tabs.forEach(tab => tab.classList.remove("active"));
   e.target.classList.add("active");
+=======
+  target_class_name = e.target.dataset["target"];
+  tabs.forEach(tab => tab.classList.remove("active"));
+>>>>>>> 7f0cddc... Clean up how tabs are being displayed
   document.querySelector(`.tab.${target_class_name}`).classList.add("active");
 }
 
