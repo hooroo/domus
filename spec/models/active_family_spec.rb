a995f49dc946196e0756108563532182422b4b93
scara82 <marta.scaramella@gmail.com>
2018-03-13 19:44:17 +1100

    Fixes mistakes from rebasing

--- spec/models/active_family_spec.rb
+++
@@ -1,8 +0,0 @@
-require "rails_helper"
-
-describe ActiveFamily do
-  describe "validations" do
-    it { is_expected.to validate_presence_of(:last_name) }
-    it { is_expected.to validate_presence_of(:capability) }
-  end
-end
