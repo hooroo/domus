65a92a4b479ce8702f9b03d94b4052c738d39b54
scara82 <marta.scaramella@gmail.com>
2018-03-16 14:44:24 +1100

    Routes in alphabetical order

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,14 @@ Rails.application.routes.draw do
   scope "(:locale)", locale: /en|it/ do
     resources :active_families, only: [:index]
     resources :all_families, only: [:index]
+    resources :all_trips, only: [:index]
+    resources :current_trips, only: [:index]
     resources :families
     resources :inactive_families, only: [:index]
     resources :schools, only: [:index, :new, :create, :edit, :update, :destroy]
     resources :trips, only: [:index, :new, :create, :edit, :update, :destroy]
-    resources :current_trips, only: [:index]
-    resources :past_trips, only: [:index]
-    resources :all_trips, only: [:index]
     resources :pages, only: [:show]
+    resources :past_trips, only: [:index]
 
     root "pages#show"
   end
